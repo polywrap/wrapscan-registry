99f82363ec861524cb731e85eb613eb0eba31db9
Jure Bogunovic <bogunovij@gmail.com>
2023-06-29 13:06:25 +0200

    added more tests

--- rust/gateway_service/src/extract_package_and_version.rs
+++ rust/gateway_service/src/extract_package_and_version.rs
@@ -1,4 +1,5 @@
-/// # Extracts the package name and version from a string.
+/// Extracts the package name and version from a string.
+/// Example: "name@version" -> ("name", Some("version"))
 pub fn extract_package_and_version(package_and_version: &str) -> (&str, Option<&str>) {
     let (package_name, version_name) = package_and_version.split_at(
         package_and_version
@@ -23,5 +24,9 @@ mod tests {
         );
         assert_eq!(extract_package_and_version("name"), ("name", None));
         assert_eq!(extract_package_and_version("name@"), ("name", None));
+        assert_eq!(extract_package_and_version("name@1.0.0"), ("name", Some("1.0.0")));
+        assert_eq!(extract_package_and_version("name@1.0.0-pre.1"), ("name", Some("1.0.0-pre.1")));
+        assert_eq!(extract_package_and_version("name-of-package@1.0.0-pre.1"), ("name-of-package", Some("1.0.0-pre.1")));
+        assert_eq!(extract_package_and_version("name_of_package@1.0.0-pre.1"), ("name_of_package", Some("1.0.0-pre.1")));
     }
 }
