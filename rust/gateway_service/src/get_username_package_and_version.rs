52078694d61f7fc19711c65f4dea0a5c5a3b4059
Jure Bogunovic <bogunovij@gmail.com>
2023-06-29 16:40:54 +0200

    formatting

--- rust/gateway_service/src/get_username_package_and_version.rs
+++ rust/gateway_service/src/get_username_package_and_version.rs
@@ -1,16 +1,16 @@
 use reqwest::StatusCode;
 
-use crate::{package_name::PackageName, extract_package_and_version, username::Username};
+use crate::{extract_package_and_version, package_name::PackageName, username::Username};
 
 pub fn get_username_package_and_version(
-  username: String,
-  package_and_version: &str,
+    username: String,
+    package_and_version: &str,
 ) -> Result<(Username, PackageName, Option<&str>), StatusCode> {
-  let username = username.parse().map_err(|_| StatusCode::BAD_REQUEST)?;
+    let username = username.parse().map_err(|_| StatusCode::BAD_REQUEST)?;
 
-  let (package_name, version_name) = extract_package_and_version(&package_and_version);
+    let (package_name, version_name) = extract_package_and_version(&package_and_version);
 
-  let package_name = package_name.parse().map_err(|_| StatusCode::BAD_REQUEST)?;
+    let package_name = package_name.parse().map_err(|_| StatusCode::BAD_REQUEST)?;
 
-  Ok((username, package_name, version_name))
+    Ok((username, package_name, version_name))
 }
